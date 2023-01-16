f475d581507a014b535d11d6b8560d89d80c25da
vitor.matias@pm.me <vitor.matias@pm.me>
2023-01-16 21:44:56 +0000

    added nostr tools and attempt to gen priv keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { generatePrivateKey, getPublicKey } from 'nostr-tools/keys';
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [privateKey, setPrivateKey] = useState(localStorage.getItem('privateKey') || '');
+  const [publicKey, setPublicKey] = useState(privateKey ? getPublicKey(privateKey) : '');
+
+
+  const handleSubmit = () => {
+    localStorage.setItem('privateKey', generatePrivateKey());
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Private Key:
+          <input type="text" value={privateKey} onChange={event => setPrivateKey(event.target.value)} />
+        </label>
+        <button type="submit">Set Private Key</button>
+        <label>
+          Public Key:
+          <input type="text" value={publicKey} onChange={event => setPublicKey(event.target.value)} />
+        </label>
+      </form>
     </div>
   )
 }
